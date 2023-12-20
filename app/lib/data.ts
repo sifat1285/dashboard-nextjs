594d18970004a93e82d1b1ddf0f2cfdc5ae307a6
sifat1285 <m.sifat1666690@gmail.com>
2023-12-20 20:39:33 +0600

    streaming done

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -36,6 +36,9 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   noStore()
   try {
+    console.log('Fetching latest invoices data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
