1ef47316a811224812e8a20af4ce43c10d3ad3bb
sifat1285 <m.sifat1666690@gmail.com>
2023-12-22 15:20:30 +0600

    fixed the key error in create-form

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -140,7 +140,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             <div id='invoice-error' aria-live='polite' aria-atomic="true">
               {state.errors?.status &&
                 state.errors.status.map((error: string) => (
-                  <p className=' mt-2 text-sm text-red-500'>{error}</p>
+                  <p key={error} className=' mt-2 text-sm text-red-500'>{error}</p>
                 ))
               }
             </div>
